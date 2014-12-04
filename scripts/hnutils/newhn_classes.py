de7334219439b5cdbf5bfb8dba7a94e766b3d315
Ross Rosen <rrosen326@gmail.com>
2014-12-04 11:18:39 -0800

    interim

--- scripts/hnutils/newhn_classes.py
+++ scripts/hnutils/newhn_classes.py
@@ -5,26 +5,34 @@ import logging
 from datetime import datetime
 from gevent.pool import Group
 from pprint import pformat
+from hn_classes import datetimeToStr
+from time import time as now
 
 
 class Stories(object):
     def __init__(self, storyids):
+        self.timestamp = now()
+        self.timestamp_str = datetimeToStr(datetime.utcfromtimestamp(self.timestamp))
+
+
         self.storyids = storyids
-        self.stories = self.getStories()
-        self.supplementStories()
+        self.postSnaps = self.getPostSnaps()
+        self.supplementPostSnaps()
+        self.doComments()
+        self.removeKids()
 
     def __len__(self):
-        return len(self.stories)
+        return len(self.postSnaps)
 
     def __repr__(self):
-        return pformat(self.stories)
+        return pformat(self.postSnaps)
 
-    def getStories(self):
+    def getPostSnaps(self):
         """
         return a list of story dicts
         """
 
-        logging.debug('getStories: about to get {0} stories'.format(len(self.storyids)))
+        logging.debug('getPostSnaps: about to get {0} postSnaps'.format(len(self.storyids)))
 
         group = Group()
         getstory = lambda storyid: firebase.get('/v0/item', storyid)
@@ -33,14 +41,37 @@ class Stories(object):
 
         return stories
 
-    def supplementStories(self):
-        # Updates stories in a story list
-        for story in self.stories:
-            story['doc_type'] = 'post' if \
+    def doComments(self):
+        # Right now, just going to set to 0
+        for postSnap in self.postSnaps:
+            postSnap['comments'] = 0
+
+    def supplementPostSnaps(self):
+        # Updates postSnaps in a story list
+        for rank, postSnap in enumerate(self.postSnaps, start=1):
+            postSnap['doc_type'] = 'post' if \
                 not config.MOCK_INPUT and not config.MOCK_OUTPUT and not config.TEST_RUN \
                 else 'test_data'
-            story['created'] = datetime.fromtimestamp(story['time'])
-            story['source'] = 'firebase'
+            postSnap['created'] = datetimeToStr(datetime.fromtimestamp(postSnap['time']))
+            postSnap['source'] = 'firebase'
+            postSnap['rank'] = rank
+            postSnap['timestamp_str'] = self.timestamp_str
+
+            if 'domain' not in postSnap:
+                postSnap['domain'] = 'NOT.SET'
+
+            # Rename to match what hind-cite expects
+            postSnap['points'] = postSnap.pop('score', None)
+            postSnap['href'] = postSnap.pop('url', None)
+            postSnap['author'] = postSnap.pop('by', None)
+
+
+
+
+    def removeKids(self):
+        for snap in self.postSnaps:
+            if 'kids' in snap:
+                del snap['kids']
 
 
 def newGetHNPosts():
